ba8ff5ae11b1777970097eb4f7206a922d0997ee
Samuel Whittenberger <sbw98978@gmail.com>
2023-10-17 21:57:45 -0400

    demo

--- src/lighting.cpp
+++ src/lighting.cpp
@@ -16,9 +16,26 @@ Lighting::~Lighting() {
 int Lighting::Load(SDL_Renderer *rend, std::vector<Resource> rs) {
     for (int i = 0; i < rs.size(); i++) {
         this->textures.push_back(new Texture{rend, rs[i]});
-        this->textures[i]->AddAnimation(new A_FromTo{ALPHA, 0, 20, i+5});
-        if (i % 2 == 0) this->textures[i]->AddAnimation(new A_FromTo{POS_X, 0, 5, 4});
-        else this->textures[i]->AddAnimation(new A_FromTo{POS_Y, 5, 0, 4});
+        if (i == 0) {
+            this->textures[i]->AddAnimation(new A_FromTo{ALPHA, 0, 20, 3});
+            this->textures[i]->AddAnimation(new A_FromTo{POS_X, 0, 5, 6});
+            this->textures[i]->AddAnimation(new A_FromTo{POS_Y, 0, 5, 6});
+        }
+        else if (i == 1) {
+            this->textures[i]->AddAnimation(new A_FromTo{ALPHA, 0, 30, 4});
+            this->textures[i]->AddAnimation(new A_FromTo{POS_Y, 7, 2, 5});
+            this->textures[i]->AddAnimation(new A_FromTo{POS_X, 7, 2, 5});
+        }
+        else if (i == 2) {
+            this->textures[i]->AddAnimation(new A_FromTo{ALPHA, 20, 10, 3});
+            this->textures[i]->AddAnimation(new A_FromTo{POS_X, 5, 0, 7});
+            this->textures[i]->AddAnimation(new A_FromTo{POS_Y, 5, 0, 7});
+        }
+        else if (i == 3) {
+            this->textures[i]->AddAnimation(new A_FromTo{ALPHA, 15, 5, 3});
+            this->textures[i]->AddAnimation(new A_FromTo{POS_Y, 0, 10, 8});
+            this->textures[i]->AddAnimation(new A_FromTo{POS_X, 0, 10, 8});
+        }
     }
 
     return 0;
